38af0ba5ddba1c60f4f62d08ba8f066f6b27ea0b
Tio da Castanha <almosttherealan@gmail.com>
2022-11-04 17:07:04 -0300

    third Update

--- src/main/scala/aprendendoScalla/Calculadora.scala
+++ src/main/scala/aprendendoScalla/Calculadora.scala
@@ -3,7 +3,11 @@ package aprendendoScalla
 object Calculadora {
   def soma(x: Int, y: Int): Int = x + y
 
+  def sub(x: Int, y: Int): Int = x - y
+
+  def mult(x: Int, y: Int): Int = x * y
+
   def + = soma _
 
-  def sub(x: Int, y: Int): Int = x - y
+  def - = sub _
 }
