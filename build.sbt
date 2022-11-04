bb591b0560d4be446b69c32e619285ac1c75f05a
Tio da Castanha <almosttherealan@gmail.com>
2022-11-04 10:27:02 -0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "HelloScala"
+
+version := "0.1"
+
+scalaVersion := "2.13.10"
---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane
+
+object Hello extends App {
+  var nome = "Alan"
+  println(s"Olá $nome Seja Bem Vindo :D")
+
+  JOptionPane.showMessageDialog(null, f"Olá $nome Seja Bem Vindo :D")
+}
