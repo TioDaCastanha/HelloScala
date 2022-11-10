b7c756c362302cc22cfb6a97e7459d816a714be4
Tio da Castanha <almosttherealan@gmail.com>
2022-11-10 20:17:14 -0300

    Lista Update

--- src/main/scala/ListaExercicios/Ex12.scala
+++ src/main/scala/ListaExercicios/Ex12.scala
@@ -1,7 +1,16 @@
 package ListaExercicios
 
+import scala.io.StdIn.readFloat
+
 object Ex12 extends App {
 
-  var salario, salarioNovo: Float = 0.0f
+  var preco, precoComDesconto: Float = 0.0f
+
+  print("Informe o valor do Produto: ")
+  preco = readFloat()
+
+  precoComDesconto = preco - (preco * 0.05).toFloat
+
+  print(s"Valor do produto com desconto de 5% é de $precoComDesconto")
 
 }
